4b18ca4bc06a6abbb66055a17f8a8d7c0874db31
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-08-10 16:02:02 -0400

    game logic implemented

--- script.js
+++ script.js
@@ -1,74 +1,145 @@
-let gameBoard = (function () {
-    let gameBoard = ['X','X','X','X','X','X','X','X','X',];
-    const fillBoard = function() {
-        const tiles = document.querySelectorAll(".tile");
-        for (let i = 0; i < tiles.length; i++) {
-            console.log(tiles[i]);
-            console.log(gameBoard[i]);
-          tiles[i].innerHTML = gameBoard[i];
-        }
-    }
-    
-    // add on click event listener to tiles
-    document.querySelectorAll();
-    // if tile is empty 
-    //   add marker
-    //   switch to next player
-    
-    const playerTurn = function() {
+// add on click event listener to tiles
 
+let gameBoard = (function () {
+  let gameBoardArray = Array(9).fill("");
+  const tiles = document.querySelectorAll(".tile");
+  const fillBoard = function () {
+    for (let i = 0; i < tiles.length; i++) {
+      // console.log(tiles[i]);
+      // console.log(gameBoard[i]);
+      if (gameBoardArray[i] === "X") {
+        tiles[
+          i
+        ].innerHTML = `<div class='tile-content x' id='${i}'>${gameBoardArray[i]}</div>`;
+      } else {
+        tiles[
+          i
+        ].innerHTML = `<div class='tile-content o' id='${i}'>${gameBoardArray[i]}</div>`;
+      }
     }
-    return {
-        fillBoard: fillBoard,
+  };
+
+  tiles.forEach((tile) => {
+    tile.addEventListener("click", addMarker);
+  });
+
+  function addMarker(e) {
+    // if tile is empty
+    if (gameBoardArray[e.target.id] === "") {
+      //add marker to gameboard array
+      //get current player
+      if (displayController.currentPlayer() === 1) {
+        gameBoardArray[e.target.id] = "X";
+      } else {
+        gameBoardArray[e.target.id] = "O";
+      }
+      fillBoard();
+      //switch to next player
+      displayController.nextTurn();
+      const winner = displayController.testGameOver();
+      console.log("winner " + winner);
+      if (winner === 1) {
+        console.log("playerOne wins")
+      }
+      else if (winner === 2)
+      {
+        console.log("playerTwo wins")
+      }
+      else if (winner === 0)
+      {
+        console.log('Tie');
+      }
     }
+  }
+
+  return {
+    fillBoard: fillBoard,
+    addMarker: addMarker,
+    gameBoardArray: () => { 
+        return gameBoardArray; }
+  };
 })();
 
 let displayController = (function () {
-    
+  let currentPlayer = 1;
+  const playerOne = document.getElementById("player-one");
+  const playerTwo = document.getElementById("player-two");
+  // switch to next player
+  function nextTurn() {
+    if (currentPlayer === 1) {
+      playerOne.classList.remove("active-turn");
+      playerTwo.classList.add("active-turn");
+      currentPlayer = 2;
+    } else {
+      playerOne.classList.add("active-turn");
+      playerTwo.classList.remove("active-turn");
+      currentPlayer = 1;
+    } // move highlight on UI
+  }
+  function testGameOver() {
+    const playerOneWin = testMarkerWin('X');
+    const playerTwoWin = testMarkerWin('O');
+    if (playerOneWin && playerTwoWin) return 0;
+    else if (playerOneWin) return 1;
+    else if (playerTwoWin) return 2;
+    else return -1;
+  }
+
+  function testMarkerWin(marker) {
+    // [0][1][2]  [3][4][5]  [6][7][8]
+    //  Rows       0,1,2 || 3,4,5 || 6,7,8
+    const board = gameBoard.gameBoardArray();
+    // console.log(board[0] + " " + board[1] + " " + board[2]);
+    if (board[0] === board[1] && board[0] === board[2]) {
+        if (board[0] === marker) return true;
+    }
+    if (board[3] === board[4] && board[3] === board[5]) {
+        if (board[3] === marker) return true;
+    }
+    if (board[6] === board[7] && board[6] === board[8]) {
+        if (board[6] === marker) return true;
+    }
+    //  Diagonals  0,4,8 || 2,4,6
+    if (board[0] === board[4] && board[0] === board[8]) {
+        if (board[0] === marker) return true;
+    }
+    if (board[2] === board[4] && board[2] === board[6]) {
+        if (board[2] === marker) return true;
+    }
+    //  Columns    0,3,6 || 1,4,7 || 2,5,8
+    if (board[0] === board[3] && board[0] === board[6]) {
+        if (board[0] === marker) return true;
+    }
+    if (board[1] === board[4] && board[1] === board[7]) {
+        if (board[1] === marker) return true;
+    }
+    if (board[2] === board[5] && board[2] === board[8]) {
+        if (board[2] === marker) return true;
+    }
+  }
+  return {
+    nextTurn: nextTurn,
+    currentPlayer: () => {
+      return currentPlayer;
+    },
+    testGameOver: testGameOver,
+  };
 })();
 
 const playerFactory = (name, marker) => {
-    // const sayHello = () => console.log('hello!');
-    return { name, marker };
+  // const sayHello = () => console.log('hello!');
+  return { name, marker, element };
 };
 
-let playerOne = {
-    name: "",
-    marker: "X"
-}
-
-let playerTwo = {
-    name: "",
-    marker: "O"
-}
-
-gameBoard.fillBoard();
-
+// gameBoard.fillBoard();
 
 /*
 TODO:
 
-Click spot to add a marker
-Switch between playerOne and playerTwo
-Players can only play empty spots
-
-
-Build the functions that allow players to add marks to a specific spot on the board, 
-and then tie it to the DOM, letting players click on the gameboard to place their marker. 
-Don’t forget the logic that keeps players from playing in spots that are already taken!
-
-
-
-Build the logic that checks for when the game is over! Should check for 
-3-in-a-row and a tie.
-
-
-Clean up the interface to allow players to put in their names, 
+Input name 
 include a button to start/restart 
-the game and add a display element that congratulates the winning player!
-
-
-
+On game over add a display element that congratulates winner
+Add confetti
 
 Scaling smoothly
 Tic tac toe wave (go down when hover over 1 of words)
@@ -92,5 +163,4 @@ probably going to want an object to control the flow of the game itself.
 
 Set up your HTML and write a JavaScript function that will render the contents of the 
 gameboard array to the webpage (for now you can just manually fill in the array with "X"s and "O"s)
-
 */
